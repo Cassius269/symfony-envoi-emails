e5f88b9567abb6f64b6124fd5b5fbc157af462ce
cassius269 <fahamygaston@gmail.com>
2024-06-02 15:06:05 +0200

    envoi d'un email avec template Twig, avec inclusion de CSS et de paramètres

--- src/Controller/EmailController.php
+++ src/Controller/EmailController.php
@@ -2,12 +2,11 @@
 
 namespace App\Controller;
 
+use Symfony\Bridge\Twig\Mime\TemplatedEmail;
 use Symfony\Component\Mime\Email;
-use Symfony\Component\Mailer\Mailer;
 use Symfony\Component\Mailer\MailerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
-use Symfony\Component\Mailer\Transport\Smtp\EsmtpTransport;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class EmailController extends AbstractController
@@ -15,17 +14,17 @@ class EmailController extends AbstractController
     #[Route('/email', name: 'emails')]
     public function index(MailerInterface $mailer): Response
     {
-        // $transport = new EsmtpTransport('smtp.mailtrap.io', 2525);
-        // $transport->setUsername('9ee42d51b4ba5f');
-        // $transport->setPassword('5eafc17ac32083');
-        // $mailer = new Mailer($transport);
-
-        $email = new Email();
+        $email = new TemplatedEmail();
         $email->from('Service Client<fahamygaston@gmail.com>')
             ->to('fahamygaston@hotmail.com')
+            ->cc("toto@gmail.com")
             ->subject('Bienvenu chez nous !')
-            ->text('Nous sommes heureux de vous accueillir')
-            ->html("<p>Bonjour</p>");
+            ->htmlTemplate('Email/welcome.html.twig')
+            ->context(
+                [
+                    'username' => 'Fahami'
+                ]
+            );
 
         $mailer->send($email);
 
